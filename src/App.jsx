9c70782d3c24e86957ddcab107e76732b0e8c6fd
Hazem AbdEl-Aziz <hazemsoliman0813@gmail.com>
2024-10-19 15:04:55 +0300

    event handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { useState } from "react"
 const App = () => {
   const [counter, setCounter ] = useState(0);
 
-  setTimeout(
-    () => setCounter(counter + 1)
-    , 1000)
   return (
     <>
     <p>{counter}</p>
+    <button onClick={() => setCounter(counter + 1)}>Plus</button>
+    <button onClick={() => setCounter(0)}>Zero</button>
     </>
   )
 }
